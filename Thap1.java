abf4918fa294bff6cdaecbbc8c10b1d1ccf7690f
ThalisonCodes <161149912+ThalisonCodes@users.noreply.github.com>
2024-03-20 14:29:14 -0400

    Add files via upload

---
+++ Thap1.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package thap1;
+
+/**
+ *
+ * @author Aluno
+ */
+import javax.swing.JFrame;
+public class Thap1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        RadioButtonFrame radioButtonFrame = new RadioButtonFrame();
+        radioButtonFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        radioButtonFrame.setSize(500,500);
+        radioButtonFrame.setVisible(true);
+    }
+    
+}
